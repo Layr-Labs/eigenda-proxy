5680be06cbb47b9c00f7307274bdf48afe3259c9
Ethen Pociask <ethen@eigenlabs.org>
2024-12-13 17:09:46 +0700

    fix: Add docker build and curl check - bump down timeout and sleep time

--- scripts/wait-for.sh
+++ scripts/wait-for.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-# poll the proxy endpoint until we get a 0 return code or 5mins have passed, in that case exit 1
-timeout_time=$(($(date +%s) + 300))
+# poll the proxy endpoint until we get a 0 return code or 2mins have passed, in that case exit 1
+timeout_time=$(($(date +%s) + 120))
 
 while (( $(date +%s) <= timeout_time )); do
   if curl -X GET 'http://localhost:6666/health'; then
     exit 0
   else
-    sleep 20
+    sleep 5
   fi
 done
 
