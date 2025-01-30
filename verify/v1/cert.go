33b2cc84d9631906e794164337bed659ff51426b
ethenotethan <ethen@eigenlabs.org>
2025-01-30 12:27:29 +0700

    feat: EigenDAV2 commitment processing and generation (#265)
    * fix(#251): better error logging for RPC lookup errors against service manager (#254)
    
    * chore: modify verifier to not require eth archive node (#241)
    
    * chore: force verifier's EthConfirmationDepth to be <64
    
    We panic in the flag's action, as well as in the verifier's constructor when this condition is not respected.
    
    This will make sure that an archival node is not required.
    
    * chore: modify verifier to load quorum parameters only once at initialization
    
    This removes the need for running with an eth archive node.
    
    * style: fix minor lint issue
    
    * docs: update README to mention that archival node is no longer needed
    
    * docs: clean-up README archival node requirement explanation
    
    * docs: fix verify/cert.go comment typo
    
    Co-authored-by: Ethen <ethen@eigenlabs.org>
    
    * docs: for eg -> e.g.
    
    * style(cert): remove unecessary bound checks from inside loop
    
    * style: create consts package with EthHappyPathFinalizationDepthBlocks = 64
    
    * style: change panic into error return
    
    * docs: change op reference for eth reference
    
    * docs: make flag comment simpler
    
    * Update verify/cert.go
    
    Co-authored-by: EthenNotEthan <ethen@eigenlabs.org>
    
    ---------
    
    Co-authored-by: Ethen <ethen@eigenlabs.org>
    
    * docs: pimp out readme with blob lifecycle diagrams (#233)
    
    * chore: move pull_request_template.md under .github/ dir
    
    * docs: reorder README sections to feel more natural (move flags to bottom)
    
    * docs (wip): add blob lifecycle diagrams to README
    
    * docs: remove Sidecar from README title (proxy is not necessarily a side)
    
    * docs: add blob lifecycle section to README
    
    * docs: add TOC to README
    
    * style(routing): rename raw_commitment -> payload
    
    We changed the name in README so should be consistent in the code
    
    * docs: update README sections to use Payload instead of Blob
    
    Posting Blobs -> Posting Payloads
    Retrieving Blobs -> Retrieving Payloads
    
    * docs: remove "obviously" word
    
    * Required quorums glitch (#255)
    
    * Avoid quorum 1 check on range of Holesky blocks
    
    * Improve SVC address check
    
    * Update verify/verifier.go
    
    Co-authored-by: Samuel Laferriere <samlaf92@gmail.com>
    
    * Update verify/verifier.go
    
    Co-authored-by: Samuel Laferriere <samlaf92@gmail.com>
    
    * Avoid unnecessary cast
    
    * Rename constant
    
    * Fix lint
    
    ---------
    
    Co-authored-by: Samuel Laferriere <samlaf92@gmail.com>
    
    * docs: update README posting payload image (#256)
    
    * fix: remove last eth_call that required archive node (#259)
    
    Forgot this one in https://github.com/Layr-Labs/eigenda-proxy/pull/241
    
    * docs: update SECURITY.md with audit commit + fix small typos (#263)
    
    * docs: update SECURITY.md
    
    * docs: update SECURITY.md with audited release number + release where findings were addressed
    
    * feat: EigenDAV2 commitment processing and generation
    
    * feat: EigenDAV2 commitment processing and generation - add note describing follow up todo
    
    ---------
    
    Co-authored-by: Samuel Laferriere <samlaf92@gmail.com>
    Co-authored-by: Gaston Ponti <pontigaston@gmail.com>

--- verify/v1/cert.go
+++ verify/v1/cert.go
@@ -4,9 +4,11 @@ import (
 	"bytes"
 	"context"
 	"fmt"
+	"math"
 	"math/big"
 	"time"
 
+	"github.com/Layr-Labs/eigenda-proxy/common/consts"
 	"github.com/Layr-Labs/eigenda/api/grpc/disperser"
 	verifier_binding "github.com/Layr-Labs/eigenda/contracts/bindings/EigenDACertVerifier"
 
@@ -24,14 +26,27 @@ import (
 // CertVerifier verifies the DA certificate against on-chain EigenDA contracts
 // to ensure disperser returned fields haven't been tampered with
 type CertVerifier struct {
-	l                    log.Logger
+	l log.Logger
+	// ethConfirmationDepth is used to verify that a blob's batch commitment has been bridged to the EigenDAServiceManager contract at least
+	// this many blocks in the past. To do so we make an eth_call to the contract at the current block_number - ethConfirmationDepth.
+	// Hence in order to not require an archive node, this value should be kept low. We force it to be < 64 (consts.EthHappyPathFinalizationDepthBlocks).
+	// waitForFinalization should be used instead of ethConfirmationDepth if the user wants to wait for finality (typically 64 blocks in happy case).
 	ethConfirmationDepth uint64
 	waitForFinalization  bool
 	manager              *svc_binding.ContractEigenDAServiceManagerCaller
 	ethClient            *ethclient.Client
+	// The two fields below are fetched from the EigenDAServiceManager contract in the constructor.
+	// They are used to verify the quorums in the received certificates.
+	// See getQuorumParametersAtLatestBlock for more details.
+	quorumsRequired           []uint8
+	quorumAdversaryThresholds map[uint8]uint8
 }
 
 func NewCertVerifier(cfg *Config, l log.Logger) (*CertVerifier, error) {
+	if cfg.EthConfirmationDepth >= uint64(consts.EthHappyPathFinalizationDepthBlocks) {
+		// We keep this low (<128) to avoid requiring an archive node.
+		return nil, fmt.Errorf("confirmation depth must be less than 64; consider using cfg.WaitForFinalization=true instead")
+	}
 	log.Info("Enabling certificate verification", "confirmation_depth", cfg.EthConfirmationDepth)
 
 	client, err := ethclient.Dial(cfg.RPCURL)
@@ -45,11 +60,18 @@ func NewCertVerifier(cfg *Config, l log.Logger) (*CertVerifier, error) {
 		return nil, err
 	}
 
+	quorumsRequired, quorumAdversaryThresholds, err := getQuorumParametersAtLatestBlock(m)
+	if err != nil {
+		return nil, fmt.Errorf("failed to fetch quorum parameters from EigenDAServiceManager: %w", err)
+	}
+
 	return &CertVerifier{
-		l:                    l,
-		manager:              m,
-		ethConfirmationDepth: cfg.EthConfirmationDepth,
-		ethClient:            client,
+		l:                         l,
+		manager:                   m,
+		ethConfirmationDepth:      cfg.EthConfirmationDepth,
+		ethClient:                 client,
+		quorumsRequired:           quorumsRequired,
+		quorumAdversaryThresholds: quorumAdversaryThresholds,
 	}, nil
 }
 
@@ -58,27 +80,22 @@ func NewCertVerifier(cfg *Config, l log.Logger) (*CertVerifier, error) {
 func (cv *CertVerifier) verifyBatchConfirmedOnChain(
 	ctx context.Context, batchID uint32, batchMetadata *disperser.BatchMetadata,
 ) error {
-	// 1. Verify batch is actually onchain at the batchMetadata's state confirmedBlockNumber.
-	// This is super unlikely if the disperser is honest, but it could technically happen that a confirmed batch's block gets reorged out,
-	// yet the tx is included in an earlier or later block, making the batchMetadata received from the disperser
-	// no longer valid. The eigenda batcher does check for these reorgs and updates the batch's confirmation block number:
-	// https://github.com/Layr-Labs/eigenda/blob/bee55ed9207f16153c3fd8ebf73c219e68685def/disperser/batcher/finalizer.go#L198
-	// TODO: We could require the disperser for the new batch, or try to reconstruct it ourselves by querying the chain,
-	// but for now we opt to simply fail the verification, which will force the batcher to resubmit the batch to eigenda.
-	confirmationBlockNumber := batchMetadata.GetConfirmationBlockNumber()
-	confirmationBlockNumberBigInt := big.NewInt(0).SetInt64(int64(confirmationBlockNumber))
-	_, err := cv.retrieveBatchMetadataHash(ctx, batchID, confirmationBlockNumberBigInt)
-	if err != nil {
-		return fmt.Errorf("batch not found onchain at supposedly confirmed block %d: %w", confirmationBlockNumber, err)
-	}
-
-	// 2. Verify that the confirmation status has been reached.
+	// 1. Verify that the confirmation status has been reached.
 	// The eigenda-client already checks for this, but it is possible for either
 	//  1. a reorg to happen, causing the batch to be confirmed by fewer number of blocks than required
 	//  2. proxy's node is behind the eigenda_client's node that deemed the batch confirmed, or
 	//     even if we use the same url, that the connection drops and we get load-balanced to a different eth node.
 	// We retry up to 60 seconds (allowing for reorgs up to 5 blocks deep), but we only wait 3 seconds between each retry,
 	// in case (2) is the case and the node simply needs to resync, which could happen fast.
+	//
+	// Note that we don't verify that the batch is actually onchain at the batchMetadata's state confirmedBlockNumber, because that would require an archive node.
+	// This is super unlikely if the disperser is honest, but it could technically happen that a confirmed batch's block gets reorged out,
+	// yet the tx is included in an earlier or later block, making the batchMetadata received from the disperser
+	// no longer valid. The eigenda batcher does check for these reorgs and updates the batch's confirmation block number:
+	// https://github.com/Layr-Labs/eigenda/blob/bee55ed9207f16153c3fd8ebf73c219e68685def/disperser/batcher/finalizer.go#L198
+	// confirmedBlockNum                               currentBlock-confirmationDepth        currentBlock
+	// | (don't verify here, need archive node)                    | (verify here)               |
+	// +-----------------------------------------------------------+-----------------------------+
 	onchainHash, err := retry.Do(ctx, 20, retry.Fixed(3*time.Second), func() ([32]byte, error) {
 		blockNumber, err := cv.getConfDeepBlockNumber(ctx)
 		if err != nil {
@@ -87,7 +104,7 @@ func (cv *CertVerifier) verifyBatchConfirmedOnChain(
 		return cv.retrieveBatchMetadataHash(ctx, batchID, blockNumber)
 	})
 	if err != nil {
-		return fmt.Errorf("retrieving batch that was confirmed at block %v: %w", confirmationBlockNumber, err)
+		return fmt.Errorf("retrieving batch that was confirmed at block %v: %w", batchMetadata.GetConfirmationBlockNumber(), err)
 	}
 
 	// 3. Compute the hash of the batch metadata received as argument.
@@ -98,12 +115,12 @@ func (cv *CertVerifier) verifyBatchConfirmedOnChain(
 		SignedStakeForQuorums: batchMetadata.GetBatchHeader().GetQuorumSignedPercentages(),
 	}
 	recordHash := [32]byte(batchMetadata.GetSignatoryRecordHash())
-	computedHash, err := HashBatchMetadata(header, recordHash, confirmationBlockNumber)
+	computedHash, err := HashBatchMetadata(header, recordHash, batchMetadata.GetConfirmationBlockNumber())
 	if err != nil {
 		return fmt.Errorf("failed to hash batch metadata: %w", err)
 	}
 
-	// 4. Ensure that hash generated from local cert matches one stored on-chain.
+	// 3. Ensure that hash generated from local cert matches one stored on-chain.
 	equal := slices.Equal(onchainHash[:], computedHash[:])
 	if !equal {
 		return fmt.Errorf("batch hash mismatch, onchain: %x, computed: %x", onchainHash, computedHash)
@@ -157,7 +174,10 @@ func (cv *CertVerifier) getConfDeepBlockNumber(ctx context.Context) (*big.Int, e
 }
 
 // retrieveBatchMetadataHash retrieves the batch metadata hash stored on-chain at a specific blockNumber for a given batchID
-// returns an error if some problem calling the contract happens, or the hash is not found
+// returns an error if some problem calling the contract happens, or the hash is not found.
+// We make an eth_call to the EigenDAServiceManager at the given blockNumber to retrieve the hash.
+// Therefore, make sure that blockNumber is <128 blocks behind the latest block, to avoid requiring an archive node.
+// This is currently enforced by having EthConfirmationDepth be <64.
 func (cv *CertVerifier) retrieveBatchMetadataHash(ctx context.Context, batchID uint32, blockNumber *big.Int) ([32]byte, error) {
 	onchainHash, err := cv.manager.BatchIdToBatchMetadataHash(&bind.CallOpts{Context: ctx, BlockNumber: blockNumber}, batchID)
 	if err != nil {
@@ -167,4 +187,45 @@ func (cv *CertVerifier) retrieveBatchMetadataHash(ctx context.Context, batchID u
 		return [32]byte{}, fmt.Errorf("BatchMetadataHash not found for BatchId %d at block %d", batchID, blockNumber.Uint64())
 	}
 	return onchainHash, nil
+}
+
+// getQuorumParametersAtLatestBlock fetches the required quorums and quorum adversary thresholds
+// from the EigenDAServiceManager contract at the latest block.
+// We then cache these parameters and use them in the Verifier to verify the certificates.
+//
+// Note: this strategy (fetching once and caching) only works because these parameters are immutable.
+// They might be different in different environments (e.g. on a devnet or testnet), but they are fixed on a given network.
+// We used to allow these parameters to change (via a setter function on the contract), but that then forced us here in the proxy
+// to query for these parameters on every request, at the batch's reference block number (RBN).
+// This in turn required rollup validators running this proxy to have an archive node, in case the RBN was >128 blocks in the past,
+// which was not ideal. So we decided to make these parameters immutable, and cache them here.
+func getQuorumParametersAtLatestBlock(
+	manager *svc_binding.ContractEigenDAServiceManagerCaller,
+) ([]uint8, map[uint8]uint8, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	requiredQuorums, err := manager.QuorumNumbersRequired(&bind.CallOpts{Context: ctx})
+	if err != nil {
+		return nil, nil, fmt.Errorf("failed to fetch QuorumNumbersRequired from EigenDAServiceManager: %w", err)
+	}
+	ctx, cancel = context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	thresholds, err := manager.QuorumAdversaryThresholdPercentages(&bind.CallOpts{Context: ctx})
+	if err != nil {
+		return nil, nil, fmt.Errorf("failed to fetch QuorumAdversaryThresholdPercentages from EigenDAServiceManager: %w", err)
+	}
+	if len(thresholds) > math.MaxUint8 {
+		return nil, nil, fmt.Errorf("thresholds received from EigenDAServiceManager contains %d > 256 quorums, which isn't possible", len(thresholds))
+	}
+	var quorumAdversaryThresholds = make(map[uint8]uint8)
+	for quorumNum, threshold := range thresholds {
+		quorumAdversaryThresholds[uint8(quorumNum)] = threshold //nolint:gosec // disable G115 // We checked the length of thresholds above
+	}
+	// Sanity check: ensure that the required quorums are a subset of the quorums for which we have adversary thresholds
+	for _, quorum := range requiredQuorums {
+		if _, ok := quorumAdversaryThresholds[quorum]; !ok {
+			return nil, nil, fmt.Errorf("required quorum %d does not have an adversary threshold. Was the EigenDAServiceManager properly deployed?", quorum)
+		}
+	}
+	return requiredQuorums, quorumAdversaryThresholds, nil
 }
