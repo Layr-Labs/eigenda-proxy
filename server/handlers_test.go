c9a2e404ee9c12aced98d249e3dfd3510a114c7d
litt3 <102969658+litt3@users.noreply.github.com>
2025-03-19 11:23:48 -0400

    Fix unit tests
    Signed-off-by: litt3 <102969658+litt3@users.noreply.github.com>

--- server/handlers_test.go
+++ server/handlers_test.go
@@ -127,6 +127,7 @@ func TestHandlerPutSuccess(t *testing.T) {
 	ctrl := gomock.NewController(t)
 	defer ctrl.Finish()
 	mockStorageMgr := mocks.NewMockIManager(ctrl)
+	mockStorageMgr.EXPECT().IsV2Enabled().AnyTimes().Return(false)
 
 	tests := []struct {
 		name         string
@@ -229,6 +230,7 @@ func TestHandlerPutErrors(t *testing.T) {
 	ctrl := gomock.NewController(t)
 	defer ctrl.Finish()
 	mockStorageMgr := mocks.NewMockIManager(ctrl)
+	mockStorageMgr.EXPECT().IsV2Enabled().AnyTimes().Return(false)
 
 	tests := []struct {
 		name                         string
