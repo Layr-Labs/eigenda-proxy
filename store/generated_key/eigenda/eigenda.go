9217fe71b7b07d54e6bc5967becad478d2786c2b
Samuel Laferriere <samlaf92@gmail.com>
2025-05-15 01:26:27 +0800

    docs: remove outdated comment

--- store/generated_key/eigenda/eigenda.go
+++ store/generated_key/eigenda/eigenda.go
@@ -181,7 +181,6 @@ func (e Store) Put(ctx context.Context, value []byte) ([]byte, error) {
 		return nil, fmt.Errorf("failed to verify commitment: %w", err)
 	}
 
-	// we set RollupL1InclusionBlockNum to 0 to skip the check, as it is meaningless in the PUT route.
 	// The cert will only be included in the batcher's inbox after
 	// the proxy returns the verified cert to the batcher.
 	err = e.verifier.VerifyCert(ctx, cert)
