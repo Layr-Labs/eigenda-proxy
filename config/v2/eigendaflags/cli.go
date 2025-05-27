bafaad40d62035e153e1abbd062562b6910caeab
Samuel Laferriere <samlaf92@gmail.com>
2025-05-27 12:07:57 +0800

    docs: full recency check eqn in flag

--- config/v2/eigendaflags/cli.go
+++ config/v2/eigendaflags/cli.go
@@ -201,7 +201,7 @@ EigenDANetwork values include %s, %s, & %s.`,
 			Name: RBNRecencyWindowSizeFlagName,
 			Usage: `Allowed distance (in L1 blocks) between the eigenDA cert's reference 
 block number (RBN) and the L1 block number at which the cert was included 
-in the rollup's batch inbox. A cert is valid when certL1InclusionBlock <= cert.RBN + rbnRecencyWindowSize, 
+in the rollup's batch inbox. A cert is valid when cert.RBN < certL1InclusionBlock <= cert.RBN + rbnRecencyWindowSize, 
 and otherwise is considered stale and verification will fail, and a 418 HTTP error will be returned.
 This check is optional and will be skipped when set to 0.`,
 			Value:    0,
