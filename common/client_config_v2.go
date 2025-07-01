9603bb5fd8fbe2c1a2d1422c5c5e4a467d7fcd32
Samuel Laferriere <samlaf92@gmail.com>
2025-07-01 22:35:52 +0800

    Update common/client_config_v2.go
    Co-authored-by: ethenotethan <ethen@eigenlabs.org>

--- common/client_config_v2.go
+++ common/client_config_v2.go
@@ -48,7 +48,7 @@ type ClientConfigV2 struct {
 	RBNRecencyWindowSize uint64
 
 	// The EigenDA network that is being used.
-	// It is optional, and when set will be used for validating that the eth-rpc chain ID matches network.
+	// It is optional, and when set will be used for validating that the eth-rpc chain ID matches the network.
 	EigenDANetwork EigenDANetwork
 }
 
